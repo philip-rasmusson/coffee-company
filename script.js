a9cf2e47f455c6863cf6073d88017ae684f83879
Philip Rasmusson <studiobigmouth@gmail.com>
2021-07-11 18:35:51 +0200

    added fontawesome

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
 const navbarMenu = () => {
   return (`
-  <h1>COFFEE COMPANY</h1>
+  <h1 id="title"><a href="#section-one">COFFEE COMPANY</a></h1>
       <div class="navbar-menu">
         <ul>
-          <li>ABOUT US</li>
-          <span> | </span>
-          <li>FIND WORK</li>
-          <span> | </span>
-          <li>GET IN CONTACT</li>
-          <span> | </span>
-          <li>ENVIROMENT</li>
+        <li><a href="#section-two">ABOUT US</a></li>
+          <li><a href="#section-three">FIND WORK</a></li>
+          <li><a href="#section-four">GET IN CONTACT</a></li>
+          <li><a href="#section-five">ENVIROMENT</a></li>
         </ul>
         </div>
         `)
 }
 
-document.getElementById('navbar').innerHTML = navbarMenu()
+document.querySelector('#navbar-one').innerHTML = navbarMenu()
+document.querySelector('#navbar-two').innerHTML = navbarMenu()
+document.querySelector('#navbar-three').innerHTML = navbarMenu()
 
      // <input placeholder="SEARCH..." />
