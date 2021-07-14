8144946fa23ac5cf58a57964fcc9f888232f4f3f
Philip Rasmusson <studiobigmouth@gmail.com>
2021-07-14 19:52:09 +0200

    full responsive, no navbar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
+const showNavbar = () => {
+  const navbar = document.querySelector('#navbar-one-small-screen')
+  navbar.className === 'font-white' ? navbar.className += ' hidden' : navbar.className = 'font-white'
+
+  console.log(navbar.className)
+}
+
+
+
+
+
 const navbarMenu = () => {
   return (`
-  
+
       <div class="navbar-menu">
         <ul>
         <li><a href="#section-two">ABOUT US</a></li>
@@ -19,9 +30,9 @@ document.querySelector('#navbar-one').innerHTML = navbarMenu()
 
 const navbarMenuSmall = () => {
   return (`
-      <div class="navbar-menu">
+      <div class="navbar-menu-small">
         <ul>
-        <li><a href="#section-two">ABOUT US</a></li>
+          <li><a href="#section-two">ABOUT US</a></li>
           <li><a href="#section-three">FIND WORK</a></li>
           <li><a href="#section-four">GET IN CONTACT</a></li>
           <li><a href="#section-five">ENVIROMENT</a></li>
